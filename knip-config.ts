5b30fc9a20d0c29cd8681d18372ae0512887fe11
Etienne FRANK <etienne.frank@bit.admin.ch>
2023-09-13 10:45:58 +0200

    Update to knip@angular

--- knip-config.ts
+++ knip-config.ts
@@ -1,13 +1,8 @@
 import { KnipConfig } from 'knip';
 
 const config: KnipConfig = {
-  entry: ['./src/main.ts'],
   ignoreDependencies: [
-    '@angular-devkit/build-angular  ',
-    '@angular/compiler-cli          ',
-    '@types/jasmine                 ',
     'jasmine-core                   ',
-    'karma                          ',
     'karma-chrome-launcher          ',
     'karma-coverage                 ',
     'karma-jasmine                  ',
